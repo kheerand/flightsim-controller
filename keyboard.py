d1033e42a3dff5234397c1878edafa9c93876706
Kheeran D <kheerand@gmail.com>
2020-02-19 13:48:41 +0000

    Initial commit

---
+++ keyboard.py
@@ -0,0 +1,48 @@
+#import RPi.GPIO as GPIO
+from gpiozero import LED, Button
+import time
+
+NULL_CHAR = chr(0)
+
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setwarnings(True)
+#GPIO.setup(23, GPIO.IN,GPIO.PUD_DOWN)
+#GPIO.setup(24, GPIO.OUT)
+switch1 = Button(23,pull_up=False)
+led = LED(24)
+
+#last_state = GPIO.input(23)
+last_state = switch1.is_pressed
+
+def send_keys(keys):
+    with open('/dev/hidg0', 'rb+') as fd:
+        fd.write(keys.encode())
+
+def switch1_actions(state):
+    if state == 1:
+        print ("Switch pressed")
+        keys = NULL_CHAR*2 + chr(22) + NULL_CHAR*5
+        led.on()
+    else:
+        print ("Switch off")
+        #keys = NULL_CHAR*2 + chr(5) + NULL_CHAR*5
+        keys = NULL_CHAR*8
+        led.off()
+    return(keys)
+
+
+
+while True:
+
+    # Switch 1
+    #current_state = GPIO.input(23)
+    current_state = switch1.is_pressed
+    if current_state != last_state:
+        #print("last state: %d" % last_state)
+        last_state = current_state
+        #print("state: %d" % current_state)
+        keys = switch1_actions(current_state)
+        send_keys(keys)
+
+    time.sleep(0.1)
+
