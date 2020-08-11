09ebbefd6e3d27327cdf4e5105db51ad0065773a
Kheeran D <kheerand@gmail.com>
2020-08-11 16:37:03 +0100

    fixed setup.sh bugs and updated .gitignore

--- setup.sh
+++ setup.sh
@@ -3,12 +3,13 @@
 echo "Install dependencies"
 echo "Installing py-gaugette and its dependencies"
 echo "   Installing wiringpi"
-sudo pip install wiringpi
+sudo pip3 install wiringpi
 
 echo "   Installing py-gaugette"
-curr_dir=`pwd`
-cd ~
+#curr_dir=`pwd`
+#cd ~
 git clone git://github.com/guyc/py-gaugette.git
-cd py-gaugette
-sudo python setup.py install
-cd $curr_dir
+ln -s py-gaugette/gaugette gaugette
+#cd py-gaugette
+#sudo python setup.py install
+#cd $curr_dir
